22566c41b607ffda1d6a2d20492f790768a854d1
Wuan_26092004 <phamhuyen1115204@gmail.com>
2025-05-07 11:26:40 +0700

    Update Model Order

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,11 +1,16 @@
 namespace MVC.Models
 {
-public class Order
-{
-    public required int OrderId {get; set;}
-    public required int CustomerId {get; set;}
-    public required DateTime OrderDate {get; set;}
-    public required decimal TotalAmount {get; set;}
-    public Customer? Customer { get; set; }
-}
+    public class Order
+    {
+        public required int OrderId { get; set; }
+        public required int CustomerId { get; set; }
+        public int? PetId { get; set; }
+        public int? ProductId { get; set; }
+        public required DateTime OrderDate { get; set; }
+        public required decimal TotalAmount { get; set; }
+
+        public Customer? Customer { get; set; }
+        public Pet? Pet { get; set; }
+        public Product? Product { get; set; }
+    }
 }
